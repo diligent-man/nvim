dd2ac760eded0169eb7e6ca5fc5f4ee503a2b1df
Alienware <trongnd02@gmail.com>
2025-07-31 15:24:04 +0700

    add strip, lstrip, strip

--- lua/utils/str_ops.lua
+++ lua/utils/str_ops.lua
@@ -6,6 +6,50 @@ local function capitalize(first_chr, rest_str)
 end
 
 
+---@param str string
+---@param strip_type string
+---@return string
+local function strip(str, strip_type)
+    strip_type = strip_type or "both"
+    assert(strip_type == "both" or
+            strip_type == "lstrip" or
+            strip_type == "rstrip",
+            "Wrong strip option"
+    )
+
+    ---@type string
+    local re = ".-"
+
+    ---@type string
+    local ws = "%s*"
+
+    if strip == "lstrip" then
+        re = string.format("^%s(%s)$", ws, re)
+    elseif strip == "rstrip" then
+        re = string.format("^(%s)%s$", re, ws)
+    else
+        re = string.format("^%s(%s)%s$", ws, re, ws)
+    end
+
+    return str:match("^%s*(.-)%s*$")
+end
+
+
+---@param str string
+---@return string
+local function lstrip(str)
+    return strip(str, "lstrip")
+end
+
+
+---@param str string
+---@return string
+local function rstrip(str)
+    return strip(str, "rstrip")
+end
+
+
+
 ---@param inp string
 ---@param delim string
 ---@param start number
@@ -30,5 +74,8 @@ end
 
 return {
     split = split,
+    strip = strip,
+    lstrip = lstrip,
+    rstrip = rstrip,
     capitalize = capitalize
 }
