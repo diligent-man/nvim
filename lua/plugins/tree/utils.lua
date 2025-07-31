94f67ed3f05a5c516052cb48bf079ca11c21cefe
Alienware <trongnd02@gmail.com>
2025-07-31 23:10:25 +0700

    update tree setup

--- lua/plugins/tree/utils.lua
+++ lua/plugins/tree/utils.lua
@@ -11,18 +11,18 @@ local function autoclose_tab_buffer(winnr)
     ]]
     local api = require"nvim-tree.api"
 
-    local tabnr = nvim_win_get_tabpage(winnr)
+    local tabnr = win_get_tabpage(winnr)
 
-    local bufnr = nvim_win_get_buf(winnr)
+    local bufnr = win_get_buf(winnr)
     local buf_info = getbufinfo(bufnr)[1]
 
-    local tab_wins = tbl_filter(function(w) return w ~= winnr end, nvim_tabpage_list_wins(tabnr))
-    local tab_bufs = tbl_map(nvim_win_get_buf, tab_wins)
+    local tab_wins = filter(function(w) return w ~= winnr end, tabpage_list_wins(tabnr))
+    local tab_bufs = map(win_get_buf, tab_wins)
 
     -- close buffer was nvim tree
     if buf_info.name:match(".*NvimTree_%d*$") then
         -- Close all nvim tree on :q
-        if not tbl_isempty(tab_bufs) then                         -- and was not the last window (not closed automatically by code below)
+        if not isempty(tab_bufs) then                             -- and was not the last window (not closed automatically by code below)
             api.tree.close()
         end
     else                                                          -- else closed buffer was normal buffer
@@ -31,10 +31,10 @@ local function autoclose_tab_buffer(winnr)
 
             if last_buf_info.name:match(".*NvimTree_%d*$") then   -- and that buffer is nvim tree
                 schedule(function ()
-                    if #nvim_list_wins() == 1 then                -- if its the last buffer in vim
+                    if #list_wins() == 1 then                -- if its the last buffer in vim
                         vicmd("quit")                             -- then close all of vim
                     else                                          -- else there are more tabs open
-                        nvim_win_close(tab_wins[1], true)         -- then close only the tab
+                        win_close(tab_wins[1], true)         -- then close only the tab
                     end
                 end)
             end
--- lua/utils/PluginManager.lua
+++ lua/utils/PluginManager.lua
@@ -88,22 +88,25 @@ function PluginManager:post_init()
     vicmd("syntax enable")
     vicmd("colorscheme dracula_pro_van_helsing_custom")
 
-
-
+    --------------------------------------------------------------------------------------------------------------------
+    --- NVim Tree ---
     set_hl(0, "NvimTreeNormal", {bg="#2B2D30"})
+    set_hl(0, "NvimTreeRootFolder", {italic=true, bold=true, fg="#00FFFF"})
 
-    set_hl(0, "NvimTreeOpenedHL", {fg = "#FFFFFF", bg = "#0B94FC"})
+    set_hl(0, "NvimTreeOpenedHL", {fg = "#FFFFFF", bg = "#0B94FC", bold=true})
 
-    vim.cmd(":hi NvimTreeSymlink gui=italic")  -- can't not set via api. Why ?
-    set_hl(0, "NvimTreeSymlinkFolderName", {fg="#9580F9"})
+
+    set_hl(0, "NvimTreeSymlink", {italic=true})
+    set_hl(0, "NvimTreeSymlinkFolderName", {fg="#9580F9", italic=true})
 
     set_hl(0, "NvimTreeFolderName", {fg="#02AEFF"})
     set_hl(0, "NvimTreeEmptyFolderName", {fg="#02AEFF"})
 
     set_hl(0, "NvimTreeGitFolderStagedHL", {fg = "#00FF00"})
     set_hl(0, "NvimTreeGitFileStagedHL", {fg = "#00FF00"})
+    --------------------------------------------------------------------------------------------------------------------
 
-    set_hl(0, "NvimTreeGitFolderDeletedHL", {fg = "#FF0000"})
+
 end
 
 
